456a0061d9dea5a3b1ceab56e2ff4428fd789d35
yum <richardminyu@foxmail.com>
2021-06-24 00:11:14 +0800

    update 006/

--- string/006Z字形变换/03.js
+++ string/006Z字形变换/03.js
@@ -11,17 +11,39 @@ const cv = require("./test");
 小结
   耗时很长，跟双循环有很大关系，下一步的优化，应该在于解决双循环的问题，更进一步，可能不需要数组，而是调整原字符串的位置。于是抛弃解法 1 中的点，重新以其索引排序：
 
-  numRows = 3
+  numRows = 3 step = 4
   转换后：
-    1，5，9，13，2，4，6，8，10，12，14，3，7，11
+    1，5，9，13
+    2，4，6，8，10，12，14，
+    3，7，11
 
-  numRows = 4
+  numRows = 4 step = 6
   转换后：
-    1，7，13，2，6，8，12，14，3，5，9，11，4，10
+    1，7，13
+    2，6，8，12，14
+    3，5，9，11
+    4，10
 
-  numRows = 6
+  numRows = 6 step = 10
   转换后：
-    1，11，2，10，12，3，9，13，4，8，14，5，7，6
+    1，11
+    2，10，12
+    3，9，13
+    4，8，14
+    5，7
+    6
+
+  numRows = 9 step = 16
+  转换后：
+    1
+    2
+    3
+    4，14
+    5，13
+    6，12
+    7，11
+    8，10
+    9
 
  */
 
@@ -36,13 +58,17 @@ var convert = function (s, numRows) {
   }
   let leng = s.length;
   let step = 2 * numRows - 2;
-  let str = "";
+  let str = '';
 
-  for (let i = 1; i <= leng; i++) {
-    str[i] = s[i - 1];
-  }
+  // for (let i = 0; i < leng; i++) {
+  //   if (i + step < leng) {
+  //     console.log(i, s[i], i + step, s[i + step]);
+  //     str = str + s[i] + s[i + step]
+  //   }
+  // }
 
-  console.log(str);
+  // console.log(s);
+  // console.log(str);
   return str;
 };
 
--- string/006Z字形变换/test.js
+++ string/006Z字形变换/test.js
@@ -6,12 +6,12 @@ const assert = require('assert').strict;
 const cv = (fn) => {
   console.log("--- is convert ---");
   assert.deepStrictEqual(fn("PAYPALISHIRING", "3"), "PAHNAPLSIIGYIR");
-  assert.deepStrictEqual(fn("PAYPALISHIRING", "4"), "PINALSIGYAHRPI");
-  assert.deepStrictEqual(fn("PAYPALISHIRING", "6"), "PRAIIYHNPSGAIL");
-  assert.deepStrictEqual(fn("PAYPALISHIRING", "8"), "PAGYNPIARLIIHS");
-  assert.deepStrictEqual(fn("PAYPALISHIRING", "10"), "PAYPALGINSIHRI");
-  assert.deepStrictEqual(fn("PAYPALISHIRING", "13"), "PAYPALISHIRIGN");
-  assert.deepStrictEqual(fn("A", "1"), "A");
+  // assert.deepStrictEqual(fn("PAYPALISHIRING", "4"), "PINALSIGYAHRPI");
+  // assert.deepStrictEqual(fn("PAYPALISHIRING", "6"), "PRAIIYHNPSGAIL");
+  // assert.deepStrictEqual(fn("PAYPALISHIRING", "8"), "PAGYNPIARLIIHS");
+  // assert.deepStrictEqual(fn("PAYPALISHIRING", "10"), "PAYPALGINSIHRI");
+  // assert.deepStrictEqual(fn("PAYPALISHIRING", "13"), "PAYPALISHIRIGN");
+  // assert.deepStrictEqual(fn("A", "1"), "A");
   console.log("--- END ---");
 }
 
