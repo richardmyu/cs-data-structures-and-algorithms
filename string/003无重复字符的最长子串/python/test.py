6c51dc562f499bd42b5be8d646a58e072927fad4
yum <richardminyu@foxmail.com>
2021-07-20 23:51:49 +0800

    update ignore: rm __pycache__

--- string/003无重复字符的最长子串/python/test.py
+++ string/003无重复字符的最长子串/python/test.py
@@ -8,8 +8,6 @@ class LongestSubstringTestCase(unittest.TestCase):
     """测试 longest_substring.py"""
 
     def test_longest_substring(self):
-        # longest_substring = length_of_longest_substring('adfasdf')
-        # self.assertEqual(longest_substring, 4)
         self.assertEqual(length_of_longest_substring("abcabcbb"), 3)
         self.assertEqual(length_of_longest_substring("bbbb"), 1)
         self.assertEqual(length_of_longest_substring("pwwkew"), 3)
