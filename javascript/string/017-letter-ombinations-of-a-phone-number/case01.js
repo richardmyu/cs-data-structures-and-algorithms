79bda9170fc393adc4622b6a9c3e5bfb6a0a3af3
yum <richardminyu@foxmail.com>
2021-12-26 18:42:53 +0800

    add 017

--- javascript/string/017-letter-ombinations-of-a-phone-number/case01.js
+++ javascript/string/017-letter-ombinations-of-a-phone-number/case01.js
@@ -2,6 +2,8 @@ const testFn = require("./test");
 
 /**
 解法 1
+  76ms, 23.93%
+  37.6MB, 91.94%
 
 思路
 
@@ -9,11 +11,55 @@ const testFn = require("./test");
  */
 
 /**
- * @param {}
- * @return {}
+ * @param {string}digits
+ * @return {string[]}
  */
-const fn = function (s) {
-
+const letterCombinations = function (digits) {
+  if (digits.length > 5 || /[^2-9]/.test(digits)) {
+    return false;
+  }
+  const leng = digits.length;
+  const mapList = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z']
+  };
+  let result = [];
+  if (leng === 0) {
+    result = [];
+  } else if (leng === 1) {
+    result = mapList[digits];
+  } else if (leng === 2) {
+    for (let i = 0; i < mapList[digits[0]].length; i++) {
+      for (let j = 0; j < mapList[digits[1]].length; j++) {
+        result.push(mapList[digits[0]][i] + mapList[digits[1]][j]);
+      }
+    }
+  } else if (leng === 3) {
+    for (let x = 0; x < mapList[digits[0]].length; x++) {
+      for (let y = 0; y < mapList[digits[1]].length; y++) {
+        for (let z = 0; z < mapList[digits[2]].length; z++) {
+          result.push(mapList[digits[0]][x] + mapList[digits[1]][y] + mapList[digits[2]][z]);
+        }
+      }
+    }
+  } else if (leng === 4) {
+    for (let a = 0; a < mapList[digits[0]].length; a++) {
+      for (let b = 0; b < mapList[digits[1]].length; b++) {
+        for (let c = 0; c < mapList[digits[2]].length; c++) {
+          for (let d = 0; d < mapList[digits[3]].length; d++) {
+            result.push(mapList[digits[0]][a] + mapList[digits[1]][b] + mapList[digits[2]][c] + mapList[digits[3]][d]);
+          }
+        }
+      }
+    }
+  }
+  return result;
 };
 
-testFn(fn,"解法 1");
+testFn(letterCombinations, "解法 1");
--- javascript/string/017-letter-ombinations-of-a-phone-number/test.js
+++ javascript/string/017-letter-ombinations-of-a-phone-number/test.js
@@ -3,9 +3,12 @@
  */
 const assert = require('assert').strict;
 
-const testFn = (fn,msg) => {
+const testFn = (fn, msg) => {
   console.log(`--- test ${msg} ---`);
-//  assert.deepStrictEqual(fn("abcd"), 1);
+  assert.deepStrictEqual(fn(""), []);
+  assert.deepStrictEqual(fn("2"), ["a", "b", "c"]);
+  assert.deepStrictEqual(fn("23"), ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]);
+  assert.deepStrictEqual(fn("234"), ["adg", "adh", "adi", "aeg", "aeh", "aei", "afg", "afh", "afi", "bdg", "bdh", "bdi", "beg", "beh", "bei", "bfg", "bfh", "bfi", "cdg", "cdh", "cdi", "ceg", "ceh", "cei", "cfg", "cfh", "cfi"]);
   console.log("all cases pass the test");
   console.log("--- END ---");
 }
