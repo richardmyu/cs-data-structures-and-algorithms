4b1a0dabc462d1cd36d09d81296bbac655912e53
yum <richardminyu@foxmail.com>
2022-01-01 01:09:40 +0800

    add 028

--- javascript/string/028-implement-strStr/case01.js
+++ javascript/string/028-implement-strStr/case01.js
@@ -2,18 +2,50 @@ const testFn = require("./test");
 
 /**
 解法 1
+  80ms, 60.75%
+  40.8MB, 5.01%
 
 思路
 
 小结
  */
-
 /**
- * @param {}
- * @return {}
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
  */
-const fn = function (s) {
-
+const strStr = function (haystack, needle) {
+  let hLeng = haystack.length;
+  let nLeng = needle.length;
+  let indexAry = [];
+  let index = 0;
+  let flag = false;
+  if (nLeng === 0) {
+    return 0;
+  }
+  if (nLeng === hLeng) {
+    if (haystack === needle) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+  for (let i = 0; i < hLeng - nLeng + 1; i++) {
+    if (haystack[i] === needle[0]) {
+      indexAry.push(i);
+    }
+  }
+  if (indexAry.length === 0) {
+    return -1;
+  }
+  for (let j = 0; j < indexAry.length; j++) {
+    if (haystack.slice(indexAry[j], indexAry[j] + nLeng) === needle) {
+      index = indexAry[j];
+      flag = true;
+      break;
+    }
+  }
+  return flag ? index : -1;
 };
 
-testFn(fn,"解法 1");
+testFn(strStr, "解法 1");
--- javascript/string/028-implement-strStr/test.js
+++ javascript/string/028-implement-strStr/test.js
@@ -3,9 +3,16 @@
  */
 const assert = require('assert').strict;
 
-const testFn = (fn,msg) => {
+const testFn = (fn, msg) => {
   console.log(`--- test ${msg} ---`);
-//  assert.deepStrictEqual(fn("abcd"), 1);
+  assert.deepStrictEqual(fn("hello", "ll"), 2);
+  assert.deepStrictEqual(fn("lhello", "ll"), 3);
+  assert.deepStrictEqual(fn("aaaaa", "bba"), -1);
+  assert.deepStrictEqual(fn("", ""), 0);
+  assert.deepStrictEqual(fn("", "a"), -1);
+  assert.deepStrictEqual(fn("a", "a"), 0);
+  assert.deepStrictEqual(fn("abc", "c"), 2);
+  assert.deepStrictEqual(fn("aaa", "a"), 0);
   console.log("all cases pass the test");
   console.log("--- END ---");
 }
