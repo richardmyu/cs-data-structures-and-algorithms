4984ed637546285bf961c0e3c9681cad0ba89ced
yum <richardminyu@foxmail.com>
2022-01-13 00:50:16 +0800

    add 043 case02

--- javascript/string/043-multiply-strings/case02.js
+++ javascript/string/043-multiply-strings/case02.js
@@ -15,7 +15,36 @@ const testFn = require("./test");
  * @return {string}
  */
 const multiply = function (num1, num2) {
+  let max_num, min_num;
+  let str = '';
+  if (num1.length >= num2.length) {
+    max_num = num1;
+    min_num = num2;
+  } else {
+    max_num = num2;
+    min_num = num1;
+  }
+  let max_num_leng = max_num.length;
+  let min_num_leng = min_num.length;
+  if (max_num_leng <= 14 && min_num_leng <= 14) {
+    let r = [];
+    for (let i = max_num_leng - 1; i >= 0; i--) {
+      r.push(Number(max_num[i]) * Number(min_num));
+    }
+    for (let j = 0; j < r.length; j++) {
+      str = r[j] + Number(str.slice(0, -j)) + str.slice(-j);
+    }
+  } else if (max_num_leng > 14 && min_num_leng <= 14) {
+    let max_num_ary = [];
+    let max_num_page = Math.floor(max_num_leng / 14);
+    for (let i = 0; i < max_num_page; i++) {
+      max_num_ary.push(max_num.slice(i * 14, (i + 1) * 14));
+    }
+    max_num_ary.push(max_num.slice(max_num_page*14));
+  } else {
 
+  }
+  return str;
 };
 
 testFn(multiply, "解法 2");
