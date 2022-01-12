3dd39db695ed723389c8f244c1a948b226beb4c6
yum <richardminyu@foxmail.com>
2022-01-12 23:27:50 +0800

    add 043

--- javascript/string/043-multiply-strings/case01.js
+++ javascript/string/043-multiply-strings/case01.js
@@ -9,11 +9,34 @@ const testFn = require("./test");
  */
 
 /**
- * @param {}
- * @return {}
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
  */
-const fn = function (s) {
+const multiply = function (num1, num2) {
+  let num = BigInt(0);
+  let mul = BigInt(0);
+  let leng = num2.length - 1;
+  for (let i = leng; i >= 0; i--) {
+    if (leng < String(Number.MAX_SAFE_INTEGER).length) {
+      console.log('-11-');
+      mul = BigInt(Math.pow(10, leng - i));
+    } else {
+      console.log('-22-');
+      let intMul = Math.ceil((leng - i) / 16);
+      let floatMul = (leng - i) % 15;
+      console.log('-22 ', intMul, floatMul);
+      for (let j = 0; j < intMul; j++) {
+        mul += BigInt(Math.pow(10, 15));
+      }
+      mul += BigInt(Math.pow(10, floatMul));
+    }
+    console.log('===', mul);
+    num = BigInt(num) + BigInt(num1) * BigInt(num2[i]) * mul;
 
+  }
+  console.log(num);
+  return num.toString();
 };
 
-testFn(fn,"解法 1");
+testFn(multiply, "解法 1");
