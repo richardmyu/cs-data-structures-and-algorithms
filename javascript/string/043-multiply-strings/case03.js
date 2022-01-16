d244039f6bf31bd1e869ec029566396ba22071de
yum <richardminyu@foxmail.com>
2022-01-16 22:46:06 +0800

    update 043 case03

--- javascript/string/043-multiply-strings/case03.js
+++ javascript/string/043-multiply-strings/case03.js
@@ -15,7 +15,23 @@ const testFn = require('./test');
  * @return {string}
  */
 const multiply = function (num1, num2) {
-
+  let str = '';
+  let step = 0;
+  for (let i = num1.length - 1; i >= 0; i--) {
+    for (let j = num2.length - 1; j >= 0; j--) {
+      console.log('--', str);
+      console.log(step);
+      console.log(Number(num1[i]) * Number(num2[j]));
+      console.log(Number(str.slice(0, step)));
+      console.log(str.slice(step - 1));
+      str = Number(num1[i]) * Number(num2[j]) + Number(str.slice(0, step - 1)) + str.slice(step - 1);
+      step++;
+      console.log('--', str);
+      console.log();
+    }
+  }
+  console.log('--str', str);
+  return str;
 };
 
 testFn(multiply, '解法 3');
