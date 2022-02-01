fa4f54b06fa73de2e51832f4290fc4d09b7039de
yum <richardminyu@foxmail.com>
2022-02-01 21:59:25 +0800

    update primary-al/01/02

---
+++ primary-algorithm/01-array/02-best-time-to-buy-and-sell-stock-II/case02.js
@@ -0,0 +1,31 @@
+const testFn = require('./test');
+
+/**
+解法 2
+  56ms, 99.59%
+  41.4MB, 6.75%
+
+思路
+
+小结
+ */
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = function (prices) {
+  if (!Array.isArray(prices)) {
+    return;
+  }
+  let r = 0;
+  for (let i = 1; i < prices.length; i++) {
+    let diff = prices[i] - prices[i - 1];
+    if (diff > 0) {
+      r = r + diff;
+    }
+  }
+  return r;
+};
+
+testFn(maxProfit, '解法 2');
