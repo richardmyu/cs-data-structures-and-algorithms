6993326ab7f2287c45f57b034b5fafd69f9ecb72
yum <richardminyu@foxmail.com>
2022-02-24 00:47:32 +0800

    update primary-al/02/02

---
+++ primary-algorithm/02-string/02-integer-reversal/case02.js
@@ -0,0 +1,55 @@
+const testFn = require('./test');
+
+/**
+解法 2
+  68ms, 95.43%
+  43.3MB, 5.00%
+
+思路
+
+小结
+ */
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const reverse = function (x) {
+  let r = [];
+  let n = 0;
+  let is_minus = false;
+  let leng = 0;
+
+  if (x < 0) {
+    r = String(x).slice(1).split('');
+    is_minus = true;
+  } else {
+    r = String(x).split('');
+  }
+
+  leng = r.length;
+
+  if (leng % 2 === 0) {
+    for (let i = 0; i < (Math.floor(leng / 2)); i++) {
+      let a = r[i];
+      let b = r[leng - 1 - i];
+      r[i] = b;
+      r[leng - 1 - i] = a;
+    }
+  } else {
+    for (let i = 0; i <= (Math.floor(leng / 2)); i++) {
+      let a = r[i];
+      let b = r[leng - 1 - i];
+      r[i] = b;
+      r[leng - 1 - i] = a;
+    }
+  }
+
+  n = is_minus ? -1 * Number(r.join('')) : Number(r.join(''));
+  n = n > (Math.pow(2, 31) - 1) ? 0 : n;
+  n = n < (Math.pow(-2, 31)) ? 0 : n;
+
+  return n;
+};
+
+testFn(reverse, '解法 2');
--- primary-algorithm/02-string/02-integer-reversal/test.js
+++ primary-algorithm/02-string/02-integer-reversal/test.js
@@ -9,6 +9,7 @@ const testFn = (fn, msg) => {
   assert.deepStrictEqual(fn(-123), -321);
   assert.deepStrictEqual(fn(120), 21);
   assert.deepStrictEqual(fn(0), 0);
+  assert.deepStrictEqual(fn(10), 1);
   assert.deepStrictEqual(fn(1534236469), 0);
   console.log('all cases pass the test');
   console.log('--- END ---');
