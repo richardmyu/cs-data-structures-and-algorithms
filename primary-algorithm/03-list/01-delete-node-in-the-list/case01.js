3ed0822c90890fabb3647e6372a036330c895697
yum <richardminyu@foxmail.com>
2022-03-05 23:40:17 +0800

    add primary-al/03/01

--- primary-algorithm/03-list/01-delete-node-in-the-list/case01.js
+++ primary-algorithm/03-list/01-delete-node-in-the-list/case01.js
@@ -2,6 +2,8 @@ const testFn = require('./test');
 
 /**
 解法 1
+  64ms, 94.18%
+  43MB, 32.33%
 
 思路
 
@@ -9,11 +11,69 @@ const testFn = require('./test');
  */
 
 /**
- * @param {}
- * @return {}
+@param {ListNode} node
+@return {void} Do not return anything, modify node in-place instead.
  */
-const fn = function (s) {
-
+const deleteNode = function (node) {
+  node.val = node.next.val;
+  node.next = node.next.next;
 };
 
-testFn(fn,'解法 1');
+// Definition for singly-linked list.
+const LinkedList = function () {
+
+  let ListNode = function (val) {
+    this.val = val;
+    this.next = null;
+  }
+  let length = 0;
+  let head = null;
+
+  this.append = function (val) {
+    let node = new ListNode(val);
+    if (head === null) {
+      head = node;
+    } else {
+      let current = head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+      length++;
+    }
+  }
+
+  this.print = function () {
+    let s = '';
+    let current = head;
+    while (current.next) {
+      s += current.val + (current.next ? ',' : '')
+    }
+    return s;
+  }
+
+  this.delete = function (node) {
+    let localNode;
+    let current = head;
+    while (current.next) {
+      if (current.val === node) {
+        localNode = current;
+      }
+    }
+    localNode.val = localNode.next.val;
+    localNode.next = localNode.next.next;
+
+    return this.print().split('');
+  }
+}
+
+const getLinkedList = function (list, node) {
+  let link = new LinkedList();
+  list.forEach(item => {
+    link.append(item)
+  });
+  console.log(link.print(), node);
+  return link.delete(node);
+}
+
+testFn(getLinkedList, '解法 1');
--- primary-algorithm/03-list/01-delete-node-in-the-list/test.js
+++ primary-algorithm/03-list/01-delete-node-in-the-list/test.js
@@ -3,9 +3,12 @@
  */
 const assert = require('assert').strict;
 
-const testFn = (fn,msg) => {
+const testFn = (fn, msg) => {
   console.log(`--- test ${msg} ---`);
-  // assert.deepStrictEqual(fn('abcd'), 1);
+  assert.deepStrictEqual(fn([4, 5, 1, 9], 1), [4, 5, 9]);
+  assert.deepStrictEqual(fn([1, 2, 3, 4], 3), [1, 2, 4]);
+  assert.deepStrictEqual(fn([0, 1], 0), [1]);
+  assert.deepStrictEqual(fn([-3, 5, -99], -3), [5, -99]);
   console.log('all cases pass the test');
   console.log('--- END ---');
 };
