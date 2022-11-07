6d4f177c27e7452c48cbe42815100169a1cb901d
yum <richardminyu@foxmail.com>
2022-11-07 23:21:32 +0800

    refactor(/): rm py script and template

--- python/string/003-longest-substring-without-repeating-characters/test.py
+++ python/string/003-longest-substring-without-repeating-characters/test.py
@@ -6,6 +6,7 @@ from case01 import length_of_longest_substring
 
 class TestStringCase(unittest.TestCase):
     def test_longest_substring(self):
+        # print(f'--- test {length_of_longest_substring.__name__} ---');
         self.assertEqual(length_of_longest_substring("abcabcbb"), 3)
         self.assertEqual(length_of_longest_substring("bbbb"), 1)
         self.assertEqual(length_of_longest_substring("pwwkew"), 3)
@@ -15,6 +16,8 @@ class TestStringCase(unittest.TestCase):
         self.assertEqual(length_of_longest_substring(" $% "), 3)
         self.assertEqual(length_of_longest_substring(" $% *&^%"), 6)
         self.assertEqual(length_of_longest_substring("abcabcd"), 4)
+        # print('all cases pass the test');
+        # print('--- END ---');
 
 
 if __name__ == '__main__':
