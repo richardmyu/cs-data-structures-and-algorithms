c89539d9d189644213259e648271d018eb8a890d
yum <richardminyu@foxmail.com>
2022-11-28 22:50:32 +0800

    feat(sql/all-q): add 265

--- sql/all-questions/sql265.sql
+++ sql/all-questions/sql265.sql
@@ -1,1 +1,30 @@
--- 
+-- SQL265 牛客每个人最近的登录日期(六)
+SELECT
+  u_n,
+  date,
+  (
+    SELECT
+      SUM(PN.number)
+    FROM
+      passing_number AS PN
+      INNER JOIN user AS U ON PN.user_id = U.id
+    WHERE
+      DAYS.date >= PN.date
+      AND DAYS.u_n = U.name
+  ) AS ps_num
+FROM
+  (
+    SELECT
+      U.name AS u_n,
+      PN.date,
+      SUM(PN.number) AS day_num
+    FROM
+      passing_number AS PN
+      INNER JOIN user AS U ON PN.user_id = U.id
+    GROUP BY
+      u_n,
+      date
+  ) AS DAYS
+ORDER BY
+  date,
+  u_n;
