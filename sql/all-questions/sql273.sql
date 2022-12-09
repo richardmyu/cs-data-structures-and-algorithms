23c3a67ba1214aa802d57ca225a85b37ecb18726
yum <richardminyu@foxmail.com>
2022-12-09 23:50:46 +0800

    feat(sql/all-q): add 273

--- sql/all-questions/sql273.sql
+++ sql/all-questions/sql273.sql
@@ -1,1 +1,33 @@
 -- SQL273 牛客的课程订单分析(三)
+SELECT
+  id,
+  user_id,
+  product_name,
+  status,
+  client_id,
+  date
+FROM
+  (
+    SELECT
+      id,
+      user_id,
+      product_name,
+      status,
+      client_id,
+      date,
+      COUNT(*) OVER (
+        PARTITION BY user_id
+        ORDER BY
+          user_id
+      ) AS cnt
+    FROM
+      order_info
+    WHERE
+      date > '2025-10-15'
+      AND status = 'completed'
+      AND product_name IN ('C++', 'Java', 'Python')
+  ) AS OED
+WHERE
+  cnt >= 2
+ORDER BY
+  id;
