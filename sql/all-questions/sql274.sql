5cd266c2161a33e8800f0c8f2d972e2a21327334
yum <richardminyu@foxmail.com>
2022-12-11 22:55:28 +0800

    feat(sql/all-q): add 274

---
+++ sql/all-questions/sql274.sql
@@ -0,0 +1,23 @@
+-- SQL274 牛客的课程订单分析(四)
+SELECT
+  user_id,
+  MIN(date) AS first_but_date,
+  COUNT(date) AS cnt
+FROM
+  (
+    SELECT
+      user_id,
+      date
+    FROM
+      order_info
+    WHERE
+      date > '2025-10-15'
+      AND status = 'completed'
+      AND product_name IN ('C++', 'Java', 'Python')
+  ) AS OO
+GROUP BY
+  user_id
+HAVING
+  cnt >= 2
+ORDER BY
+  user_id;
