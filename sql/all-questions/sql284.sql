1b73cb24342d41e5d666bddc497147db83e27ea5
yum <richardminyu@foxmail.com>
2022-12-25 19:54:44 +0800

    feat(sql/all-q): add 284

---
+++ sql/all-questions/sql284.sql
@@ -0,0 +1,30 @@
+-- SQL284 获得积分最多的人(二)
+SELECT
+  U.id,
+  U.name,
+  GI.grade_num
+FROM
+  user AS U
+  INNER JOIN (
+    SELECT
+      user_id,
+      grade_num,
+      DENSE_RANK() OVER(
+        ORDER BY
+          grade_num DESC
+      ) AS ranking
+    FROM
+      (
+        SELECT
+          user_id,
+          SUM(grade_num) AS grade_num
+        FROM
+          grade_info
+        GROUP BY
+          user_id
+      ) AS SU
+  ) AS GI ON U.id = GI.user_id
+WHERE
+  ranking = 1
+ORDER BY
+  id;
