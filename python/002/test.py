16639196d9bc08c548df6d4a5a45d7eae7fa31e2
yum <richardminyu@foxmail.com>
2023-05-18 19:28:27 +0800

    python(leetcode/): 002

--- python/002/test.py
+++ python/002/test.py
@@ -5,11 +5,7 @@ from case01 import Solution
 class TestCase(unittest.TestCase):
     def test_solution(self):
         solution = Solution()
-        self.assertEqual(solution.addTwoNumbers([2, 7, 11, 15], 9), [0, 1])
-        self.assertEqual(solution.addTwoNumbers([2, 11, 7, 15], 9), [0, 2])
-        self.assertEqual(solution.addTwoNumbers([2, 11, 15, 7], 9), [0, 3])
-        self.assertEqual(solution.addTwoNumbers([3, 2, 4], 6), [1, 2])
-        self.assertEqual(solution.addTwoNumbers([3, 3], 6), [0, 1])
+        self.assertEqual(solution.addTwoNumbers(l1=[2, 4, 3], l2=[5, 6, 4]), [7, 0, 8])
 
 
 if __name__ == '__main__':
