0bbd1ea4eb644ed2b675c1de35d80f5aa85bd19e
yum <richardminyu@foxmail.com>
2023-05-18 19:14:54 +0800

    python(leetcode/): 001

--- python/001/case01.py
+++ python/001/case01.py
@@ -3,10 +3,16 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        idxi = 0
+        idxj = 0
+
         for i, val in enumerate(nums):
             for j in range(i + 1, len(nums)):
                 if val + nums[j] == target:
-                    return [i, j]
+                    idxi = i
+                    idxj = j
+                    break
+        return [idxi, idxj]
 
 
 # solution = Solution()
