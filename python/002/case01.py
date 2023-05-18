74fe2db847ba7b59bcd1d62c84d696190c70d16b
yum <richardminyu@foxmail.com>
2023-05-18 19:27:09 +0800

    python(leetcode/): 002

---
+++ python/002/case01.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+from typing import Optional
+from list_note import ListNode
+
+
+class Solution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        print(l1)
+        for i in l1:
+            print('--i', i)
+
+
+soulution = Solution()
+
+print(soulution.addTwoNumbers(l1=[2, 4, 3], l2=[5, 6, 4]))
