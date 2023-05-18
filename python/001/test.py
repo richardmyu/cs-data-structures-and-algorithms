aa8e1a5ff79c1e7d6b7d1d6e8d04ab004054223f
yum <richardminyu@foxmail.com>
2023-05-18 19:07:09 +0800

    python(leetcode/): 001

--- python/001/test.py
+++ python/001/test.py
@@ -6,6 +6,8 @@ class TestCase(unittest.TestCase):
     def test_solution(self):
         solution = Solution()
         self.assertEqual(solution.twoSum([2, 7, 11, 15], 9), [0, 1])
+        self.assertEqual(solution.twoSum([2, 11, 7, 15], 9), [0, 2])
+        self.assertEqual(solution.twoSum([2, 11, 15, 7], 9), [0, 3])
         self.assertEqual(solution.twoSum([3, 2, 4], 6), [1, 2])
         self.assertEqual(solution.twoSum([3, 3], 6), [0, 1])
 
