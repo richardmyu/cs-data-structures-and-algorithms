5d4971cea134045bc433e35fe489f61166e19714
yum <richardminyu@foxmail.com>
2023-06-04 20:27:34 +0800

    feat(python/leetcode/e-a): el08

---
+++ python/elementary-algorithm/el08.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def move_zeroes(self, nums: List[int]) -> None:
+        for i in nums:
+            if i == 0:
+                nums.remove(i)
+                nums.append(0)
+        return nums
+
+
+solution = Solution()
+print(solution.move_zeroes([0]))
+# [0]
+print(solution.move_zeroes([0, 0, 1]))
+# [1, 0, 0]
+print(solution.move_zeroes([0, 1, 0, 3, 12]))
+# [1, 3, 12, 0, 0]
+print(solution.move_zeroes([0, 1, 4, 0, 3, 12]))
+# [1, 4, 3, 12, 0, 0]
