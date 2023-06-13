24217c658edf4e5f5905b100609781442b8301f6
yum <richardminyu@foxmail.com>
2023-06-13 22:22:49 +0800

    feat(python/leetcode/e-a): str01

---
+++ python/elementary-algorithm/string/str_01.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def reverse_string(self, s: List[str]) -> None:
+        # case 01
+        length = len(s)
+
+        for i in range(int(len(s) / 2)):
+            s[i], s[length - 1 - i] = s[length - 1 - i], s[i]
+
+        return s
+
+
+solution = Solution()
+print(solution.reverse_string(["h", "e", "l", "l", "o"]))
+# ["o", "l", "l", "e", "h"]
+print(solution.reverse_string(["H", "a", "n", "n", "a", "h"]))
+# ["h", "a", "n", "n", "a", "H"]
